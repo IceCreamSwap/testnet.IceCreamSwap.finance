dc6a33d295a8c8f4ff4f15b1cd9a15c81eaa29b0
Cookie's Ice Cream <78191425+IceCreamSwap@users.noreply.github.com>
2021-03-25 08:57:49 -0300

    testnet frontend

---
+++ static/js/10.c6845095.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@icecreamswap/interface"]=this["webpackJsonp@icecreamswap/interface"]||[]).push([[10],{1197:function(e,c){}}]);
+//# sourceMappingURL=10.c6845095.chunk.js.map
---
+++ static/js/9.f67617a3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@icecreamswap/interface"]=this["webpackJsonp@icecreamswap/interface"]||[]).push([[9],{1071:function(e,c){}}]);
+//# sourceMappingURL=9.f67617a3.chunk.js.map
