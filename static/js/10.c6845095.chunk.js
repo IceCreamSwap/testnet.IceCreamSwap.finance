fa537f5834a5748d161037bab42090a4437f02c8
Cookie's Ice Cream <78191425+IceCreamSwap@users.noreply.github.com>
2021-05-12 19:54:20 -0300

    Automated mainnet deployment.

--- static/js/10.c6845095.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@icecreamswap/interface"]=this["webpackJsonp@icecreamswap/interface"]||[]).push([[10],{1197:function(e,c){}}]);
-//# sourceMappingURL=10.c6845095.chunk.js.map
--- static/js/9.f67617a3.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@icecreamswap/interface"]=this["webpackJsonp@icecreamswap/interface"]||[]).push([[9],{1071:function(e,c){}}]);
-//# sourceMappingURL=9.f67617a3.chunk.js.map
